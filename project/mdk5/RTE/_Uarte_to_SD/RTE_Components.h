978bd685c9182784951465f29664539423d6104b
Koen <koen.cai@cyweemotion.com>
2022-04-20 16:45:06 +0800

    fix[drv] Change of Project Name

---
+++ project/mdk5/RTE/_Uarte_to_SD/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Uart_to_SD' 
+ * Target:  'Uarte_to_SD' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "nrf.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
